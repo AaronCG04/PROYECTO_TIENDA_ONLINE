d846e66888a27eaa05bff33c2bb8d281ea615ddb
Miguel_Angel <73071287+Miguel-Angel-Mejia-Antonio@users.noreply.github.com>
2021-06-21 10:06:17 -0500

    Add files via upload

---
+++ login_registro/loginregistro/php/login_usuario.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start();
+
+include 'conexion.php';
+
+$correo=$_POST['correo'];
+$contrasena=$_POST['contrasena'];
+
+$validar_login=mysqli_query($conexion, "SELECT * FROM  cliente WHERE correo='$correo' and contrasena='$contrasena'");
+
+if (mysqli_num_rows($validar_login)>0) {
+	$_SESSION['usuario']=$correo;
+	header("location: ../bienvenido.php");
+	exit;
+}else {
+	echo '
+		<script>
+			alert("Usuario o contraseña incorrecta, por favor verifique los datos introducidos");
+			window.location="../index.php";
+			</script>
+		';
+		exit;
+}
+
+?>
